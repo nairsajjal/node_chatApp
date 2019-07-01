9e628eb4655422b9ea340c0e91deb3937af5c4a9
sajjal <nairsajjal@gmail.com>
2019-07-01 16:35:09 +0530

    final commit

--- server.js
+++ server.js
@@ -1,63 +1,63 @@
 const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
 
-//connect to mongoDB
-
+// Connect to mongo
 mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
     if(err){
         throw err;
     }
 
+    console.log('MongoDB connected...');
 
-    console.log('Mongo connected..');
-
-    //connect to socket.io
+    // Connect to Socket.io
     client.on('connection', function(socket){
         let chat = db.collection('chats');
 
-        //create function to send status
-        sendStatus = function(){
-            socket.emit('status',s);
-
+        // Create function to send status
+        sendStatus = function(s){
+            socket.emit('status', s);
         }
-        //get chats from mongo collection
+
+        // Get chats from mongo collection
         chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
             if(err){
                 throw err;
             }
+
             // Emit the messages
             socket.emit('output', res);
         });
-        //handle input events
+
+        // Handle input events
         socket.on('input', function(data){
             let name = data.name;
-            
             let message = data.message;
 
-            //check for name and message
-            if(name =='' || message==''){
-                //send error status
-                sendStatus('please enter a name and message');
+            // Check for name and message
+            if(name == '' || message == ''){
+                // Send error status
+                sendStatus('Please enter a name and message');
+            } else {
+                // Insert message
+                chat.insert({name: name, message: message}, function(){
+                    client.emit('output', [data]);
 
-            }
-            else{
-                //inser message
-                chat.insert({name:name, message: message}, function(){
-                    client.emit('output',[data]);
-                    //send status obj
+                    // Send status object
                     sendStatus({
-                        message: 'Message Sent',
+                        message: 'Message sent',
                         clear: true
-                    })
+                    });
                 });
             }
         });
-        //clear handle
+
+        // Handle clear
         socket.on('clear', function(data){
-            //remove all chats from collection
+            // Remove all chats from collection
             chat.remove({}, function(){
+                // Emit cleared
                 socket.emit('cleared');
             });
-        })
+        });
     });
 });
